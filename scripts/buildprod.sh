1a2c0b1ac83b005ba98d3ba29ab5f76d0f17b68c
Sianwa Atemi <sa.atemi@gmail.com>
2025-10-17 14:24:55 +0300

    Added tailwind build script and cloud run

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Build CSS first
+npx tailwindcss -i ./static/css/input.css -o ./static/css/output.css --minify
+
+
 # Enable CGO for SQLite support
 CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o my-journal
